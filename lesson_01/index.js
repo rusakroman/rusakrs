302888311ff51d8a3b3429fab35ca1656554531e
rusakroman <rrusak87@gmail.com>
2024-09-22 22:15:27 +0300

    начал изучать гит

---
+++ lesson_01/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const productName = "Бананы";
+const productCount = 10;
+const productCategory = "Фрукты";
+const productPrice = 120;
+
+const productTotalPrice = productPrice * productCount;
+
+console.log(productName);
+console.log('Общая цена за товары:', productTotalPrice);
